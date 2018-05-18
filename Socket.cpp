de433a5ca184d2e0fd57677f97f6bfe5116923e9
geekbutton <panpeike1234@163.com>
2018-05-18 15:40:49 +0800

    5_18

--- Socket.cpp
+++ Socket.cpp
@@ -1,8 +1,11 @@
 #include "Socket.h"
 #include <sys/types.h>
 #include <sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
 #include <netdb.h>
 #include <string.h>
+#include <iostream>
 
 int Socket(char* hostname,int host_port) {
 	//一个包裹函数，包含了socket和connect两个行为
@@ -14,12 +17,21 @@ int Socket(char* hostname,int host_port) {
 	server_addr.sin_port = htons(host_port);
 
 	
-	//当前仅考虑了hostname为字符串的情况
-	struct hostent *temp_host;
-	temp_host = gethostbyname(hostname);
-	if (temp_host == NULL)
-		return -1;
-	memcpy(&server_addr.sin_addr, *(temp_host->h_addr_list), temp_host->h_length);
+	//综合考虑URL中主机名的情况(可以是DNS中的主机名或IP地址)
+	in_addr_t temp_addr = inet_addr(hostname);
+	if (temp_addr != INADDR_NONE) {					//URI中为IP地址
+		server_addr.sin_addr.s_addr = temp_addr;
+	}
+	else {											//URI中为域名
+		struct hostent *temp_host;			
+		temp_host = gethostbyname(hostname);
+		if (temp_host == NULL)
+			return -1;
+		fprintf(stdout, "%s\n", temp_host->h_name);
+		//复制主机的IP地址到对应结构体中
+		memcpy(&server_addr.sin_addr, *(temp_host->h_addr_list), temp_host->h_length);
+	}
+	//fprintf(stdout, "%s\n", inet_ntoa(server_addr.sin_addr));
 	if (connect(sockfd, (sockaddr*) &server_addr, sizeof(server_addr)) < 0)
 		return -1; 
 	return sockfd;
