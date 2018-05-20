03e704d9420531cbaae510a52559f2b07677ce13
geekbutton <panpeike1234@163.com>
2018-05-20 16:53:40 +0800

    5_20 删除了针对IP格式输入的针对性处理，gethostbyname足够处理这种情况

--- Socket.cpp
+++ Socket.cpp
@@ -18,19 +18,19 @@ int Socket(char* hostname,int host_port) {
 
 	
 	//综合考虑URL中主机名的情况(可以是DNS中的主机名或IP地址)
-	in_addr_t temp_addr = inet_addr(hostname);
-	if (temp_addr != INADDR_NONE) {					//URI中为IP地址
-		server_addr.sin_addr.s_addr = temp_addr;
-	}
-	else {											//URI中为域名
+	//in_addr_t temp_addr = inet_addr(hostname);
+	//if (temp_addr != INADDR_NONE) {					//URI中为IP地址
+	//	server_addr.sin_addr.s_addr = temp_addr;
+	//}
+	//else {											//URI中为域名
 		struct hostent *temp_host;			
 		temp_host = gethostbyname(hostname);
 		if (temp_host == NULL)
 			return -1;
-		fprintf(stdout, "%s\n", temp_host->h_name);
+		//fprintf(stdout, "%s\n", temp_host->h_name);
 		//复制主机的IP地址到对应结构体中
 		memcpy(&server_addr.sin_addr, *(temp_host->h_addr_list), temp_host->h_length);
-	}
+	//}
 	//fprintf(stdout, "%s\n", inet_ntoa(server_addr.sin_addr));
 	if (connect(sockfd, (sockaddr*) &server_addr, sizeof(server_addr)) < 0)
 		return -1; 
